8d9ab6ed48d153324192c8906d0a20a02773ae7f
LukasKarer <lukas@karer.tech>
2025-05-29 22:01:34 +0200

    bugfix1

--- karer_drescher_ordner/java_macht_keinen_spaß/src/App.java
+++ karer_drescher_ordner/java_macht_keinen_spaß/src/App.java
@@ -1,9 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
+	System.out.println("Hello, World! bugfix");
         System.out.println("Hello, World!");
     }
 
     public void thisIsANewMethod() {
         System.out.println("This is a new method");
+	System.out.println("This is a new methodBUGFIX");
      }
 }
